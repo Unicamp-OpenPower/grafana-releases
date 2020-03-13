7309b2414a6dfb55d9e3ab2f13cc9434845bdf7b
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-03-13 11:34:19 -0300

    Create ftp_version.py

---
+++ ftp_version.py
@@ -0,0 +1,30 @@
+import requests
+import re
+
+# find and save the current Github release
+html = str(
+    requests.get('https://github.com/grafana/grafana/releases/latest')
+    .content)
+index = html.find('Release ')
+github_version = html[index + 9:index + 14]
+file = open('github_version.txt', 'w')
+file.writelines(github_version)
+file.close()
+
+# find and save the current Bazel version on FTP server
+html = str(
+    requests.get(
+        'https://oplab9.parqtec.unicamp.br/pub/ppc64el/grafana/'
+    ).content)
+index = html.rfind('grafana-')
+ftp_version = html[index + 8:index + 13]
+file = open('ftp_version.txt', 'w')
+file.writelines(ftp_version)
+file.close()
+
+# find and save the oldest Bazel version on FTP server
+index = html.find('grafana-')
+delete = html[index + 8:index + 13]
+file = open('delete_version.txt', 'w')
+file.writelines(delete)
+file.close()
