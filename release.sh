bbf487912bba6d7f152cbf641f35b81f066d7578
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-08-28 19:54:02 -0300

    Release done

--- release.sh
+++ release.sh
@@ -11,21 +11,17 @@ then
   cd repository-scrips/
   chmod +x empacotar-deb.sh
   chmod +x empacotar-rpm.sh
-  #mv empacotar-deb.sh $BINPATH
-  #mv empacotar-rpm.sh $BINPATH
-  #cd $BINPATH
-
-  wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/grafana/grafana-7.1.5-cli
-  wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/grafana/grafana-7.1.5-server
-
-  #./empacotar-deb.sh grafana-cli grafana-$github_version-cli $github_version " "
-  #./empacotar-deb.sh grafana-server grafana-$github_version-server $github_version " "
+  mv empacotar-deb.sh $BINPATH
+  mv empacotar-rpm.sh $BINPATH
+  cd $BINPATH
+  ./empacotar-deb.sh grafana-cli grafana-$github_version-cli $github_version " "
+  ./empacotar-deb.sh grafana-server grafana-$github_version-server $github_version " "
   sudo ./empacotar-rpm.sh grafana-cli grafana-$github_version-cli $github_version " " "The tool for beautiful monitoring and metric analytics & dashboards for Graphite, InfluxDB & Prometheus & More"
   sudo ./empacotar-rpm.sh grafana-server grafana-$github_version-server $github_version " " "The tool for beautiful monitoring and metric analytics & dashboards for Graphite, InfluxDB & Prometheus & More"
   if [ $github_version > $ftp_version ]
   then
-    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/debian/ppc64el/grafana/ /var/lib/jenkins/workspace/grafana-releases/grafana/bin/linux-ppc64le/grafana-cli-$github_version-ppc64le.deb"
-    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/debian/ppc64el/grafana/ /var/lib/jenkins/workspace/grafana-releases/grafana/bin/linux-ppc64le/grafana-server-$github_version-ppc64le.deb"
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/debian/ppc64el/grafana/ /var/lib/jenkins/workspace/grafana-releases/grafana/bin/linux-ppc64le/grafana-cli-$github_version-ppc64le.deb"
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/debian/ppc64el/grafana/ /var/lib/jenkins/workspace/grafana-releases/grafana/bin/linux-ppc64le/grafana-server-$github_version-ppc64le.deb"
     sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/rpm/ppc64le/grafana/ ~/rpmbuild/RPMS/ppc64le/grafana-cli-$github_version-1.ppc64le.rpm"
     sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/rpm/ppc64le/grafana/ ~/rpmbuild/RPMS/ppc64le/grafana-server-$github_version-1.ppc64le.rpm"
   fi
